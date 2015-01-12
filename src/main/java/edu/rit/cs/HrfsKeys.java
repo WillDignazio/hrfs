d46a7819c8a2c6762019f79ba8394878ff89c037
Will Dignazio <wdignazio@gmail.com>
2015-01-12 13:01:18 -0300

    configuration: Remove unecessary configuration keys
    Remove keys that are no longer necessary for the code base, this
    includes keys which were for gathering a multicast address for the
    cluster.

--- src/main/java/edu/rit/cs/HrfsKeys.java
+++ src/main/java/edu/rit/cs/HrfsKeys.java
@@ -15,13 +15,9 @@ public final class HrfsKeys
 	public static final String HRFS_NODE_ADDRESS	= "hrfs.node.address";
 	public static final String HRFS_NODE_PATH	= "hrfs.node.path";
 	public static final String HRFS_NODE_PORT	= "hrfs.node.port";
-	public static final String HRFS_NODE_GROUP_ADDRESS	= "hrfs.node.group.address";
-	public static final String HRFS_NODE_GROUP_PORT		= "hrfs.node.group.port";
 
 	public static final String HRFS_ZOOKEEPER_ADDRESS	= "hrfs.zookeeper.address";
 	public static final String HRFS_ZOOKEEPER_PORT		= "hrfs.zookeeper.port";
-
-	public static final String HRFS_BLKSZ		= "hrfs.blksz";
 
 	/* Assinged Value Constants */
 	public static final String HRFS_DEFAULT_URI_SCHEME	= "hrfs";
